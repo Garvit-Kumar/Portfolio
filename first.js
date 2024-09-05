badc4d9d6dbc5ab071ca5dfc5550e24d67563e3d
Garvit-Kumar <garvitchaudhary534@gmail.com>
2024-09-05 23:16:31 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,17 @@
+function showContent() {
+    let root = document.getElementById("root");
+    if (root.style.display === 'none' || root.style.display === '') {
+        root.style.display = 'block';
+    } else {
+        root.style.display = 'none';
+    }
+}
+
+
+function goo(){
+    window.open("https://drive.google.com/file/d/16VzKykNqHMhenzFrZ4ogYCt0aStkHTq9/view?usp=drivesdk", '_blank');
+}
+
+function Email(){
+    
+}
