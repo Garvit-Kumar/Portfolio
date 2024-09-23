c57ff810bae31d45e8347921723c853a3f9fe089
Garvit-Kumar <garvitchaudhary534@gmail.com>
2024-09-24 00:32:03 +0530

    Update first.js

--- first.js
+++ first.js
@@ -13,6 +13,30 @@ function goo(){
 }
 
 function Email(){
-     window.open('mailto:garvitchaudhary534@gmail.com')
     
+}
+// ---------------------------------------------------------------------------------------------------------
+
+
+(function(){
+    emailjs.init("2sKhzBfEoqLLrQxOD"); 
+})();
+
+function sendEmail(event) {
+    event.preventDefault(); 
+
+    var params = {
+        from_name: document.getElementById('name').value,
+        email_id: document.getElementById('email').value,
+        message: document.getElementById('message').value
+    };
+
+    emailjs.send("service_evtm13q", "template_pry1kg9", params)
+        .then((response) => {
+            document.getElementById('msg').innerText = "Email sent successfully!";
+            document.getElementById('contact-form').reset(); 
+        }, (error) => {
+            document.getElementById('msg').innerText = "Failed to send email. Please try again.";
+            console.error('Error sending email:', error);
+        });
 }
